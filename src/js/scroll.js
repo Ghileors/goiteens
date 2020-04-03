333c4696eaaaf25cbe2c8aa5815a82b780388e30
Ghileors <58727406+Ghileors@users.noreply.github.com>
2020-04-03 14:46:37 +0300

    Refactored project

---
+++ src/js/scroll.js
@@ -0,0 +1,14 @@
+document.querySelector(".hero__button").onclick = () => {
+	const height = document.querySelector(".hero").offsetHeight;
+	window.scrollTo({
+	  	top: height,
+	  	behavior: "smooth",
+	});
+};
+
+document.querySelector(".btn-up__link").onclick = () => {
+	window.scrollTo({
+	  	top: 0,
+	  	behavior: "smooth",
+	});
+};
